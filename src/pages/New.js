8a69816a0e58de72177fd1b8dd40c620250ce6ba
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-08-07 01:44:32 +0800

    refactor: new review page

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,9 +1,42 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import ReviewEditor from '../components/ReviewEditor';
 
 const New = () => {
+  const [isBookSelected, setIsBookSelected] = useState(false);
+  const [bookData, setBookData] = useState(false);
+  const { id } = useParams();
+  const defaultData = {
+    bookId: null,
+    title: 'Title',
+    author: 'Author',
+    thumbnail: 'no thumbnail',
+  };
+
+  useEffect(() => {
+    if (id) {
+      fetch(`https://www.googleapis.com/books/v1/volumes/${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setBookData({
+            bookId: data.id,
+            title: data.volumeInfo.title,
+            author: data.volumeInfo.authors,
+            thumbnail: data.volumeInfo.imageLinks
+              ? data.volumeInfo.imageLinks.thumbnail
+              : 'no thumbnail',
+          });
+        });
+      setIsBookSelected(true);
+    }
+  }, []);
+
   return (
     <div>
-      <ReviewEditor></ReviewEditor>
+      <ReviewEditor
+        isEdit={false}
+        data={isBookSelected ? bookData : defaultData}
+      ></ReviewEditor>
     </div>
   );
 };
