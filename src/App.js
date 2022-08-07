87fd41b51418c27438d7ce74b9c4ade4814294ae
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-08-07 16:40:25 +0800

    feat: Store data to local storage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useReducer, useRef } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { ReviewStateContext } from './context/ReviewStateContext';
 import { ReviewDispatchContext } from './context/ReviewDispatchContext';
 
@@ -12,27 +12,46 @@ import New from './pages/New';
 import Review from './pages/Review';
 
 const reducer = (state, action) => {
+  let newState = [];
   switch (action.type) {
     case 'INIT':
       return action.data;
     case 'CREATE':
-      return [action.data, ...state];
+      newState = [action.data, ...state];
+      break;
     case 'EDIT':
-      return state.map(
+      newState = state.map(
         (it) => (it.id === action.data.id ? { ...it, ...action.data } : it)
         // Overwrite readingDate, rate, content on the existing state
       );
+      break;
     case 'DELETE':
-      return state.filter((it) => it.id !== action.targetId);
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
     default:
       return state;
   }
+  // Store data to local storage
+  localStorage.setItem('reviewList', JSON.stringify(newState));
+  return newState;
 };
 
 function App() {
   const [reviews, dispatch] = useReducer(reducer, []);
 
   const reviewId = useRef(0);
+
+  useEffect(() => {
+    // Get saved data from local storage
+    const localData = localStorage.getItem('reviewList');
+    if (localData) {
+      const reviewList = JSON.parse(localData).sort(
+        (a, b) => parseInt(b.id) - parseInt(a.id)
+      );
+      reviewId.current = parseInt(reviewList[0].id) + 1;
+      dispatch({ type: 'INIT', data: reviewList });
+    }
+  }, []);
 
   const onCreate = (
     bookId,
